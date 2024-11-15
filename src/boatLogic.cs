05222dfde1e8055f5348ece2dc57cd3c0bad26ad
maladarix <maladarix@gmail.com>
2024-11-15 10:32:35 -0500

    boatInit

---
+++ src/boatLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.src
+{
+    internal class boatLogic
+    {
+        public static void PlaceBoat(int x, int y, bool vertical, int boatLenght)
+        {
+
+        }
+    }
+}
