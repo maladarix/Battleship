01104adf39dbd2b86f5fba769020c0fcf371a702
Anthony <anthony1997lemon@gmail.com>
2024-11-18 11:14:21 -0500

    game struct et boat struct

---
+++ src/Boat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.src
+{
+    public struct Boat
+    {
+        public string Name;
+        public int Length;
+        public int FirstX = -1;
+        public int FirstY = -1;
+        public int LastX = -1;
+        public int LastY = -1;
+        public int Hp;
+
+        public Boat(string BoatName, int BoatLegnth)
+        {
+            Name = BoatName;
+            Length = BoatLegnth;
+            Hp = BoatLegnth;
+        }
+    }
+}
